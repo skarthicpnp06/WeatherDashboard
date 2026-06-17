66590a990b7cf91b043fdf9a40b0078dd17666f4
skarthicpnp06 <skarthicpnp06@gmail.com>
2026-06-17 20:37:14 +0530

    last commit may be

--- Backend/src/main/java/com/example/Backend/Controller/WeatherController.java
+++ Backend/src/main/java/com/example/Backend/Controller/WeatherController.java
@@ -2,8 +2,17 @@ package com.example.Backend.Controller;
 
 import java.util.List;
 import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.Backend.Model.AlertEntity;
 import com.example.Backend.Model.WeatherEntity;
 import com.example.Backend.Service.AlertService;
